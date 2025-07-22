58ece085a09f74b23c7ba0d7e67518b44be7a68b
Aliffaturahman <aliffaturahman@upi.edu>
2025-07-22 22:12:24 +0700

    rename file index

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -22,7 +22,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return view('customer.index');
+        return view('customer.Index');
     }
 
     /**
